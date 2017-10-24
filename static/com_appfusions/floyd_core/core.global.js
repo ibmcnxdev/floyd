db9bbc05c804535f333e7c53325ccd2620efe46a
Daniele Vistalli <dan@appfusions.com>
2017-10-24 21:36:29 +0200

    (B) Improvements and fixes (D) More documentation on how to build community apps definitions (E) Improved Caddyfile

--- static/com_appfusions/floyd_core/core.global.js
+++ static/com_appfusions/floyd_core/core.global.js
@@ -1,11 +1,10 @@
-var __floyd = null;
 var __floydHooks = __floydHooks || [];
 /*
     AppFusions Floyd Core
 
     Author: Daniele Vistalli <dan@appfusions.com>
 */
-(function(floydGlobal, hooks) {
+(function(hooks) {
 
     var floyd = {
         version : '0.0.1alpha',
@@ -13,9 +12,7 @@ var __floydHooks = __floydHooks || [];
         debugEnabled : true,
 
         debug: function(message) {
-
             if (!this.debugEnabled) return;
-
             console.log("Floyd.DEBUG " + message);
         },
 
@@ -56,7 +53,7 @@ var __floydHooks = __floydHooks || [];
                 // {
                 //      callback: function()                                                // Mandatory
                 //      query: xpath query selector for item to wait for                    // Mandatory
-                //      root: undefined or additional selector to narrow down the query     // Optional | undefined
+                //      root: undefined or additional selector to narrow down the DOM query // Optional | undefined
                 //      retries: how main retries to do before giving up                    // Optional | undefined
                 //      timeout: the delay in milliseconts between DOM scans                // Optional | undefined
                 // }
@@ -137,20 +134,80 @@ var __floydHooks = __floydHooks || [];
 
 
             },
+        },
+        context : {
+
+            is : function(contextQuestion) {
+
+                // Default is to return false
+                return false;
+            },
+            get : function(contextInformation) {
+
+                // Default is to return null
+                return null;
+            },
+            matches : function(matcher, valueToMatch) {
+
+                // Default is to return false
+                return false;
+            }
+
+        },
+        plugins : {
+            /*
+            *   Plugins are a type of extension for floyd which is being designed.
+            *   Ideally plugins will fall in the following categories
+            *
+            *   - Command plugins performing specific actions / patterns by name (eg. floyd.doCommand('commandname',parametersObject )
+            *   - Context detection plugins. Functions that will extract from browser state information on context.
+            *       eg.
+                    floyd.context.is('<checkname>') returns true/false.
+
+                    floyd.context.is('homepage')
+                    floyd.context.is('communities')
+                    floyd.context.is('communities/my')
+                    floyd.context.is('communities/member')
+                    floyd.context.is('communities/all')
+                    floyd.context.is('communities/invites')
+                    floyd.context.is('communities/community')
+                    floyd.context.is('communities/community/<uuid>')
+                    floyd.context.is('profiles/mine')
+                    floyd.context.is('profiles/others')
+                    floyd.context.is('profiles/others/sameorg')
+                    floyd.context.is('profiles/others/otherorg')
+                    floyd.context.is('profiles/search')
+
+                    floyd.context.get('<context information>') returns a value
+
+                    floyd.context.get('cnx.appname') returns the app ('homepage','profiles', 'activities' etc)
+
+                    floyd.context.matches('<matcher>','<value to match>') returns true/false. applies on the client side the 
+                        same matching logic that customizer uses on the server. Allows for fine-grain, client side decisions.
+
+                    Context detection plugins will declare the scopes they cover at registration time.
+                    A scope is a "segment" in the query namespace. ()
+
+            */
+            registerPlugin : function(pluginName,  pluginObject) {
+                /*
+                * This will register in a safe way features updates into the Floyd plugin registry and into other handlers
+                */
+            }
         }
 
     }
 
     // If dojo.define is available register floyd as a module under the name 'floyd'.
-    if (dojo && dojo.define) {
-        dojo.define('floyd',floyd);
+    if (define) {
+        define('floyd',floyd);
     }
 
     // Also make floyd available in the global namespace as __floyd.
-    floydGlobal = floyd;
+    window.__floyd = floyd;
     
     // As soon as loaded process the hooks queue
     floyd.start();
 
-})(__floyd,__floydHooks);
+})(__floydHooks);
 
--- static/com_appfusions/floyd_samples/profiles/profiles_demo.js
+++ static/com_appfusions/floyd_samples/profiles/profiles_demo.js
@@ -4,14 +4,15 @@ var __floydHooks = __floydHooks || [];
     console.log("Floyd - profiles demo loaded");
 
     hooks.push(function(floyd) {
-        // floyd.loader.loadCss('/files/muse-static/com_appfusions/floyd_samples/profiles/css/profilesCustomization.css?repoName=floyd');
+        // floyd.loader.loadCss('/files/muse-static/com_appfusions/floyd_samples/profiles/css/profilesCustomization.css');
     })
     hooks.push({ 
         query: '#friendsSection', 
         callback: function(floyd) {
-            floyd.apps.runApplication('#socialTagsSection', 'before', '/files/muse-static/com_appfusions/floyd_samples/profiles/social/index.html?repoName=floyd', '100%', '40px', false, false, { test: 'blue' } );
-            floyd.apps.runApplication('#profileHeader', 'after', '/files/muse-static/com_appfusions/floyd_samples/common/index.html?repoName=floyd', '100%', '40px', false, false, { test: 'blue' } );
-            floyd.apps.runApplication('#friendsSection', 'after', '/files/muse-static/com_appfusions/floyd_samples/profiles/myPayroll/index.html?repoName=floyd', '100%', '200px', false, false, { test: 'blue' } );
+            floyd.apps.runApplication('#socialTagsSection', 'before', '/files/muse-static/com_appfusions/floyd_samples/profiles/social/index.html', '100%', '40px', false, false, { test: 'blue' } );
+            floyd.apps.runApplication('#profileHeader', 'after', '/files/muse-static/com_appfusions/floyd_samples/common/index.html', '100%', '40px', false, false, { test: 'blue' } );
+            floyd.apps.runApplication('#friendsSection', 'after', '/files/muse-static/com_appfusions/floyd_samples/profiles/myPayroll/index.html', '100%', '200px', false, false, { test: 'blue' } );
+            floyd.apps.runApplication('#friendsSection', 'after', '/files/muse-static/com_appfusions/floyd_samples/profiles/myLearning/index.html', '100%', '200px', false, false, { test: 'blue' } );
         } 
     });
 
